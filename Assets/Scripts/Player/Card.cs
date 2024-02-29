d46b69d4d5edc4a5422383962df64b47c92bca63
Runnan55 <Daniel3323709@gmail.com>
2024-02-29 11:35:13 +0100

    Player fix + Cards

---
+++ Assets/Scripts/Player/Card.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using System.Collections;
+public class Card : MonoBehaviour
+{
+    public string CardName;
+    public float attractionRadius = 5f; // Radio de atracci�n
+    public float attractionForce = 10f; // Fuerza de atracci�n
+    // M�todo para cambiar la posici�n entre el jugador y la carta
+    public void Activate()
+    {
+
+        switch (CardName)
+        {
+            case "TpCard":
+                // Llama a la funci�n para manejar TpCard
+                TpCard();
+                Destroy(this.gameObject);
+                break;
+            case "BlackHoleCard":
+                // Llama a la funci�n para manejar BlackHoleCard
+                print("First");
+                StartCoroutine(AttractCharacters());
+                break;
+            default:
+                // Maneja el caso en el que el nombre de la carta no coincide con las opciones esperadas
+                Debug.Log("Error de carta");
+                break;
+        }
+
+    }
+    public void TpCard()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+    CharacterController controller = player.GetComponent<CharacterController>();
+
+    // Desactivar el CharacterController
+    if (controller != null) controller.enabled = false;
+
+    // Lógica para cambiar la posición aquí
+
+
+        // Encuentra el jugador en la escena usando el tag
+        GameObject playerParent = GameObject.FindGameObjectWithTag("Player");
+        // Intenta encontrar el hijo espec�fico dentro del jugador
+        Transform playerGroundTransform = playerParent?.transform.Find("PlayerGround");
+
+        GameObject card = GameObject.FindGameObjectWithTag("Card");
+
+        // Verifica que el jugador y la carta existan en la escena
+        if (playerParent != null && playerGroundTransform != null && card != null)
+        {
+            // Calcula la diferencia de posici�n entre el jugador y el PlayerGround
+            Vector3 offset = playerParent.transform.position - playerGroundTransform.position;
+
+            // Almacena temporalmente la posici�n de la carta
+            Vector3 cardPosition = card.transform.position;
+
+            // Cambia la posici�n del jugador a la posici�n de la carta, ajustada por el offset
+            playerParent.transform.position = cardPosition + offset;
+
+            // Opcionalmente, puedes mover la carta a la posici�n original del PlayerGround o realizar otras acciones
+            card.transform.position = playerGroundTransform.position; // Esto mover�a la carta a donde estaba originalmente el PlayerGround
+            Debug.Log("TpCard");
+                // Reactivar el CharacterController
+    if (controller != null) controller.enabled = true;
+        }
+        else
+        {
+            // Si no se encuentra el jugador, el hijo espec�fico del jugador o la carta, muestra un mensaje de error
+            Debug.LogError("Player, PlayerGround, or Card not found in the scene.");
+            Debug.Log("Player: " + playerParent);
+            Debug.Log("PlayerGround: " + playerGroundTransform);
+            Debug.Log("Card: " + card);
+                // Reactivar el CharacterController
+    if (controller != null) controller.enabled = true;
+        }
+    }
+
+    public void BlackHoleCard()
+    {
+        StartCoroutine(AttractCharacters());
+    }
+    IEnumerator AttractCharacters()
+    {
+        float elapsedTime = 0f;
+        float duration = 3f; // Duraci�n de la atracci�n
+
+        while (elapsedTime < duration)
+        {
+            // Encuentra todos los objetos dentro del radio de atracci�n
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, attractionRadius);
+            foreach (var hitCollider in hitColliders)
+            {
+                CharacterController controller = hitCollider.GetComponent<CharacterController>();
+                if (controller != null)
+                {
+                    // Calcula la direcci�n hacia el centro de la carta
+                    Vector3 direction = (transform.position - controller.transform.position).normalized;
+                    // Aplica la fuerza de atracci�n
+                    controller.Move(direction * attractionForce * Time.deltaTime);
+                }
+            }
+
+            elapsedTime += Time.deltaTime;
+            yield return null; // Espera hasta el pr�ximo frame
+        }
+        print("finish");
+        Destroy(this.gameObject);
+    }
+
+    // Utilizado para dibujar un gizmo en el Editor que representa el radio de atracci�n
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, attractionRadius);
+    }
+}
---
+++ Assets/Scripts/Player/ItemPlacer.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+
+public class ItemPlacer : MonoBehaviour
+{
+    public GameObject itemPrefab; // Prefab del item final
+    public GameObject previewPrefab; // Prefab de la previsualización permitida
+    public GameObject invalidPreviewPrefab; // Prefab de la previsualización no permitida
+    public LayerMask placementLayer; // Layer Mask para el suelo
+    public LayerMask obstructionLayer; // Layer Mask para detectar obstrucciones
+    public Transform player; // Referencia pública al jugador
+
+    private GameObject currentPreviewInstance; // Instancia actual de la previsualización
+    private GameObject placedObject; // Referencia al objeto colocado
+    private bool isPreviewValid = false; // Indica si la previsualización actual es válida
+    private LineRenderer lineRenderer; // LineRenderer para dibujar el trail
+    private bool isPlacementMode = true; // Controla si el modo actual es de colocación
+
+    void Start()
+    {
+        // Intenta obtener el LineRenderer del jugador, si no existe, créalo.
+        lineRenderer = player.gameObject.GetComponent<LineRenderer>();
+        if (lineRenderer == null)
+        {
+            lineRenderer = player.gameObject.AddComponent<LineRenderer>();
+            // Configura el LineRenderer aquí
+            lineRenderer.startWidth = 0.05f;
+            lineRenderer.endWidth = 0.1f;
+            lineRenderer.material = new Material(Shader.Find("Sprites/Default")); // Asegúrate de asignar un material adecuado
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) // Al presionar el botón derecho
+        {
+            if (isPlacementMode)
+            {
+                if (currentPreviewInstance == null)
+                {
+                    CreateOrUpdatePreview(previewPrefab);
+                }
+            }
+            else
+            {
+                Card cardComponent = placedObject.GetComponent<Card>();
+                if (cardComponent != null)
+                {
+                    cardComponent.Activate();
+                    // Acción de activación, eliminar el objeto colocado y volver al modo de colocación
+                    Debug.Log("Activación");
+                }
+                if (placedObject != null)
+                {
+                    //Destroy(placedObject); // Elimina el objeto colocado
+                    placedObject = null;
+                }
+                isPlacementMode = true; // Vuelve al modo de colocación
+            }
+        }
+
+        if (isPlacementMode && currentPreviewInstance != null)
+        {
+            UpdatePreviewPositionAndStatus();
+            DrawTrailToPreview(); // Dibuja el trail hacia el prefab
+        }
+        else if (lineRenderer != null && !Input.GetMouseButton(1))
+        {
+            lineRenderer.enabled = false; // Oculta el trail si no hay previsualización
+        }
+
+        if (Input.GetMouseButtonUp(1) && isPlacementMode) // Intentar colocar el item al soltar, solo si está en modo de colocación
+        {
+            PlaceItemAndClearPreview();
+        }
+    }
+
+    void CreateOrUpdatePreview(GameObject prefab)
+    {
+        if (currentPreviewInstance == null || currentPreviewInstance.GetComponentInChildren<MeshRenderer>().gameObject != prefab)
+        {
+            Vector3 position = Vector3.zero;
+            Quaternion rotation = Quaternion.identity;
+            if (currentPreviewInstance != null)
+            {
+                position = currentPreviewInstance.transform.position;
+                rotation = currentPreviewInstance.transform.rotation;
+                Destroy(currentPreviewInstance);
+            }
+            currentPreviewInstance = Instantiate(prefab, position, rotation);
+        }
+    }
+
+    void UpdatePreviewPositionAndStatus()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, placementLayer))
+        {
+            currentPreviewInstance.transform.position = hit.point;
+            bool isObstructed = Physics.CheckSphere(hit.point, 0.5f, obstructionLayer);
+            GameObject prefabToUse = isObstructed ? invalidPreviewPrefab : previewPrefab;
+            if (isObstructed != isPreviewValid || currentPreviewInstance.GetComponentInChildren<MeshRenderer>().gameObject != prefabToUse)
+            {
+                isPreviewValid = !isObstructed;
+                CreateOrUpdatePreview(prefabToUse);
+            }
+        }
+    }
+
+    void DrawTrailToPreview()
+    {
+        if (lineRenderer != null && currentPreviewInstance != null)
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, player.position);
+            lineRenderer.SetPosition(1, currentPreviewInstance.transform.position);
+        }
+    }
+
+    void PlaceItemAndClearPreview()
+    {
+        if (isPreviewValid && currentPreviewInstance != null)
+        {
+            placedObject = Instantiate(itemPrefab, currentPreviewInstance.transform.position, Quaternion.identity);
+            isPlacementMode = false; // Desactiva el modo de colocación después de colocar un objeto
+        }
+        if (currentPreviewInstance != null)
+        {
+            Destroy(currentPreviewInstance);
+            currentPreviewInstance = null;
+        }
+        if (lineRenderer != null)
+        {
+            lineRenderer.enabled = false; // Oculta el trail después de colocar el item
+        }
+    }
+}
