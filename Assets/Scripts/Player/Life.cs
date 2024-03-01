e31dd78cd4ae91ab63f8c5ca31038bcfd7c4692e
nefeliii <albertocovelovalverde@gmail.com>
2024-03-01 22:11:08 +0100

    vida

---
+++ Assets/Scripts/Player/Life.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour
+{
+    public float maxTime = 600;
+    private float currentTime;
+
+    public TextMeshProUGUI timeText;
+    public Image timeImage;
+
+    void Start()
+    {
+        currentTime = maxTime;
+        timeImage.fillAmount = 1;
+    }
+
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            UpdateTimeText();
+            UpdateTimeImage();
+        }
+        else
+            Debug.Log("muerte");
+
+        //* DEBUG
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ModifyTime(60);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ModifyTime(-60);
+        }
+    }
+
+    public void ModifyTime(float amount)
+    {
+        currentTime += amount;
+        if (currentTime > maxTime)
+        {
+            currentTime = maxTime;
+        }
+        else if (currentTime < 0)
+        {
+            currentTime = 0;
+        }
+        UpdateTimeImage();
+    }
+
+    private void UpdateTimeText()
+    {
+        int minutes = Mathf.FloorToInt(currentTime / 60);
+        int seconds = Mathf.FloorToInt(currentTime % 60);
+        timeText.text = minutes + ":" + seconds.ToString("00");
+    }
+
+    private void UpdateTimeImage()
+    {
+        timeImage.fillAmount = currentTime / maxTime;
+    }
+}
