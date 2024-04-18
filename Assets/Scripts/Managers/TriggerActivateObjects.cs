b16f3ae369a45a80742304f7221bd5a5875d86d8
Luisiitt <144843710+Luisiitt@users.noreply.github.com>
2024-04-18 11:40:57 +0200

    walls arena

---
+++ Assets/Scripts/Managers/TriggerActivateObjects.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerActivateObjects : MonoBehaviour
+{
+    public GameObject objectToActivate;
+    public float delayInSeconds = 120f; // 120 segundos = 2 minutos
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Comprueba si el objeto que entra al trigger es el jugador
+        if (other.CompareTag("Player"))
+        {
+            // Activa los GameObjects
+            ActivateObjects(true);
+
+            // Planifica la desactivación de los GameObjects
+            Invoke(nameof(DeactivateObjects), delayInSeconds);
+        }
+    }
+
+    private void ActivateObjects(bool isActive)
+    {
+        if (objectToActivate != null)
+            objectToActivate.SetActive(isActive);
+
+    }
+
+    private void DeactivateObjects()
+    {
+        ActivateObjects(false);
+    }
+}
