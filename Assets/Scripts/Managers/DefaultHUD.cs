3f610b5db27ad6d14b68aeb194b73a80ac74582b
nefeliii <albertocovelovalverde@gmail.com>
2024-09-17 16:47:05 +0200

    Fixeo fmod?

--- Assets/Scripts/Managers/DefaultHUD.cs
+++ Assets/Scripts/Managers/DefaultHUD.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DefaultHUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string startSceneName;
 
-    // Update is called once per frame
-    void Update()
+    public void StartButton()
     {
-        
+        SceneManager.LoadScene(startSceneName);
     }
 }
