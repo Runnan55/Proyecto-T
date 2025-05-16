5b5e2b23ca79805179149f08c7771451e36774c4
Luisiitt <144843710+Luisiitt@users.noreply.github.com>
2025-05-16 19:12:51 +0200

    arte boss

--- Assets/Scripts/Level/Boss/BossFallingTrap.cs
+++ Assets/Scripts/Level/Boss/BossFallingTrap.cs
@@ -73,7 +73,7 @@ public class BossFallingTrap : MonoBehaviour
             Vector3 projectileMoveDirection = (projectileVector - transform.position).normalized;
 
             GameObject tempProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.LookRotation(projectileMoveDirection));
-            tempProjectile.GetComponent<FlechaTest>().speed = explosionForce;
+            tempProjectile.GetComponent<ProyectilClones>().speed = explosionForce;
 
             angle += angleStep;
         }
